e24507f32c8f652e0511f3f594085fdfbe6d7d79
gene-yoo <gene.y.yoo@gmail.com>
2017-12-14 12:10:36 -0500

    Add initial render of coin market table

--- src/components/CoinList.js
+++ src/components/CoinList.js
@@ -1,11 +1,56 @@
 import React from "react";
+import { Table, Image } from "semantic-ui-react";
 
 const CoinList = props => {
 	console.log("inside coin list, render");
 	console.log("props: ", props);
 	console.log("--------------------------------------");
 
-	return <div>Inside Coin List</div>;
+	let coins = "Loading ...";
+
+	if (props.marketData.DISPLAY) {
+		coins = props.allCoins.slice(0, 5).map(coin => {
+			let coinData = props.marketData.DISPLAY[coin.Symbol]["USD"];
+			return (
+				<Table.Row key={coin.Id}>
+					<Table.Cell>{coin.SortOrder}</Table.Cell>
+					<Table.Cell verticalAlign="middle">
+						<Image
+							src={`https://www.cryptocompare.com${coin.ImageUrl}`}
+							style={{ maxWidth: "20px" }}
+						/>
+						{coin.CoinName}
+					</Table.Cell>
+					<Table.Cell>{coin.Symbol}</Table.Cell>
+					<Table.Cell>{coin.TotalCoinSupply}</Table.Cell>
+					<Table.Cell>{coinData.MKTCAP}</Table.Cell>
+					<Table.Cell>{coinData.PRICE}</Table.Cell>
+					<Table.Cell>{coinData.CHANGE24HOUR}</Table.Cell>
+					<Table.Cell>{coinData.HIGH24HOUR}</Table.Cell>
+					<Table.Cell>{coinData.LOW24HOUR}</Table.Cell>
+				</Table.Row>
+			);
+		});
+	}
+
+	return (
+		<Table textAlign="left" size="large">
+			<Table.Header>
+				<Table.Row>
+					<Table.HeaderCell>Rank</Table.HeaderCell>
+					<Table.HeaderCell>Name</Table.HeaderCell>
+					<Table.HeaderCell>Symbol</Table.HeaderCell>
+					<Table.HeaderCell>Total Coin Supply</Table.HeaderCell>
+					<Table.HeaderCell>Market Cap</Table.HeaderCell>
+					<Table.HeaderCell>Price</Table.HeaderCell>
+					<Table.HeaderCell>Change (24 Hrs)</Table.HeaderCell>
+					<Table.HeaderCell>High (24 Hrs)</Table.HeaderCell>
+					<Table.HeaderCell>Low (24 Hrs)</Table.HeaderCell>
+				</Table.Row>
+			</Table.Header>
+			<Table.Body>{coins}</Table.Body>
+		</Table>
+	);
 };
 
 export default CoinList;
