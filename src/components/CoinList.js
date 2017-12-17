b1bf6ac6892a45aba0f8210eae84419a9a49dd3e
gene-yoo <gene.y.yoo@gmail.com>
2017-12-17 14:55:46 -0500

    Update coinlist to semi-smart class

--- src/components/CoinList.js
+++ src/components/CoinList.js
@@ -1,73 +1,83 @@
-import React from "react";
+import React, { Component } from "react";
 import { Table, Image } from "semantic-ui-react";
 
-const CoinList = props => {
-	console.log("inside coin list, render");
-	console.log("props: ", props);
-	console.log("--------------------------------------");
-
-	let coins = "Loading ...";
-
-	if (props.marketData.DISPLAY) {
-		coins = props.allCoins.slice(0, 49).map(coin => {
-			let coinData = props.marketData.DISPLAY[coin.Symbol]["USD"];
-			let priceChangeStatus =
-				props.marketData.RAW[coin.Symbol]["USD"]["CHANGE24HOUR"] > 0
-					? "green"
-					: "red";
-			let selected = props.selectedSym === coin.Symbol ? "#E3F6FC" : "white";
-
-			return (
-				<Table.Row
-					key={coin.Id}
-					style={{
-						height: "50px",
-						backgroundColor: selected
-					}}
-					onClick={() => props.handleChartSelection(coin.Symbol)}
-				>
-					<Table.Cell>{coin.SortOrder}</Table.Cell>
-					<Table.Cell verticalAlign="middle">
-						<Image
-							src={`https://www.cryptocompare.com${coin.ImageUrl}`}
-							style={{
-								maxWidth: "30px",
-								paddingRight: "10px",
-								display: "inline-block"
-							}}
-						/>
-						{coin.CoinName}
-					</Table.Cell>
-					<Table.Cell>{coin.Symbol}</Table.Cell>
-					<Table.Cell>{coinData.MKTCAP}</Table.Cell>
-					<Table.Cell>{coinData.PRICE}</Table.Cell>
-					<Table.Cell style={{ color: priceChangeStatus }}>
-						{coinData.CHANGE24HOUR}
-					</Table.Cell>
-					<Table.Cell>{coinData.HIGH24HOUR}</Table.Cell>
-					<Table.Cell>{coinData.LOW24HOUR}</Table.Cell>
-				</Table.Row>
-			);
-		});
+class CoinList extends Component {
+	constructor(props) {
+		super(props);
 	}
 
-	return (
-		<Table selectable textAlign="left" style={{ width: "1000px" }}>
-			<Table.Header>
-				<Table.Row style={{ height: "25px" }}>
-					<Table.HeaderCell>Rank</Table.HeaderCell>
-					<Table.HeaderCell>Name</Table.HeaderCell>
-					<Table.HeaderCell>Symbol</Table.HeaderCell>
-					<Table.HeaderCell>Market Cap</Table.HeaderCell>
-					<Table.HeaderCell>Price</Table.HeaderCell>
-					<Table.HeaderCell>Change (24 Hrs)</Table.HeaderCell>
-					<Table.HeaderCell>High (24 Hrs)</Table.HeaderCell>
-					<Table.HeaderCell>Low (24 Hrs)</Table.HeaderCell>
-				</Table.Row>
-			</Table.Header>
-			<Table.Body>{coins}</Table.Body>
-		</Table>
-	);
-};
+	renderCoins() {
+		let coins = "Loading ...";
+
+		if (this.props.marketData.DISPLAY) {
+			coins = this.props.allCoins.slice(0, 49).map(coin => {
+				let coinData = this.props.marketData.DISPLAY[coin.Symbol]["USD"];
+				let priceChangeStatus =
+					this.props.marketData.RAW[coin.Symbol]["USD"]["CHANGE24HOUR"] > 0
+						? "green"
+						: "red";
+				let selected =
+					this.props.selectedSym === coin.Symbol ? "#E3F6FC" : "white";
+
+				return (
+					<Table.Row
+						key={coin.Id}
+						style={{
+							height: "50px",
+							backgroundColor: selected
+						}}
+						onClick={() => this.props.handleChartSelection(coin.Symbol)}
+					>
+						<Table.Cell>{coin.SortOrder}</Table.Cell>
+						<Table.Cell verticalAlign="middle">
+							<Image
+								src={`https://www.cryptocompare.com${coin.ImageUrl}`}
+								style={{
+									maxWidth: "30px",
+									paddingRight: "10px",
+									display: "inline-block"
+								}}
+							/>
+							{coin.CoinName}
+						</Table.Cell>
+						<Table.Cell>{coin.Symbol}</Table.Cell>
+						<Table.Cell>{coinData.MKTCAP}</Table.Cell>
+						<Table.Cell>{coinData.PRICE}</Table.Cell>
+						<Table.Cell style={{ color: priceChangeStatus }}>
+							{coinData.CHANGE24HOUR}
+						</Table.Cell>
+						<Table.Cell>{coinData.HIGH24HOUR}</Table.Cell>
+						<Table.Cell>{coinData.LOW24HOUR}</Table.Cell>
+					</Table.Row>
+				);
+			});
+		}
+
+		return coins;
+	}
+
+	render() {
+		console.log("inside coin list, render");
+		console.log("props: ", this.props);
+		console.log("--------------------------------------");
+		return (
+			<Table selectable textAlign="left" style={{ width: "1000px" }}>
+				<Table.Header>
+					<Table.Row style={{ height: "25px" }}>
+						<Table.HeaderCell>Rank</Table.HeaderCell>
+						<Table.HeaderCell>Name</Table.HeaderCell>
+						<Table.HeaderCell>Symbol</Table.HeaderCell>
+						<Table.HeaderCell>Market Cap</Table.HeaderCell>
+						<Table.HeaderCell>Price</Table.HeaderCell>
+						<Table.HeaderCell>Change (24 Hrs)</Table.HeaderCell>
+						<Table.HeaderCell>High (24 Hrs)</Table.HeaderCell>
+						<Table.HeaderCell>Low (24 Hrs)</Table.HeaderCell>
+					</Table.Row>
+				</Table.Header>
+				<Table.Body>{this.renderCoins()}</Table.Body>
+			</Table>
+		);
+	}
+}
 
 export default CoinList;
